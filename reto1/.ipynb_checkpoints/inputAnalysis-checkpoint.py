803b7c4215ecfe5ecb26dc2791de27a866b35659
Ciro Alberto Gamboa Entralgo <ciro.gamboa.2013@upb.edu.co>
2020-03-02 17:46:49 -0500

    Lectura de datasets

---
+++ reto1/.ipynb_checkpoints/inputAnalysis-checkpoint.py
@@ -0,0 +1,25 @@
+"""
+Hackaton 2020
+Reto 1:
+Porcentaje de afinidad: Candidato vs Vacante
+Magneto empleos es una plataforma que permite a personas de todos los sectores laborales encontrar
+empleos a nivel nacional e internacional y actualmente cuenta con una bolsa de candidatos disponibles
+para que grandes empresas encuentren los mejores perfiles que se ajusten a sus vacantes.
+ 
+Esta bolsa de candidatos ha ido creciendo de manera exponencial durante los últimos tiempos. 
+Para cada candidato recopilamos desde información personal como: nombres, apellidos, email, teléfonos,
+ entre otros, hasta información relevante para el proceso de reclutamiento como los estudios, 
+ habilidades personales, experiencia y perfil laboral. Aunque toda esta información es posible 
+ llenarla por cada candidato, es permitido ingresar en Magneto Empleos candidatos con tan solo el 
+ nombre y el correo electrónico y de manera fácil importarlos a una vacante específica.
+ 
+Esto lleva a que tanto los candidatos con información acorde a la vacante, como candidatos con la 
+información incompleta y los candidatos con información poco relevante para la vacante en particular, 
+se vean de la misma manera y mezclados entre sí, lo que hace que la búsqueda de perfiles que se 
+ajusten con la vacante sean una tarea larga, dispendiosa y costosa.
+"""
+
+#%% Importar librerias
+import pandas as pd
+
+
